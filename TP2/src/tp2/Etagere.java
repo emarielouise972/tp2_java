40692faf2d86bad56eb62f5fef274204af7d4f63
YoannGit <lehongcheffsonyoann@gmail.com>
2025-10-31 16:36:47 +0100

    Ajout des question 5 à 10 de la Partie 2

--- TP2/src/tp2/Etagere.java
+++ TP2/src/tp2/Etagere.java
@@ -1,85 +1,139 @@
 package tp2;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author jerem
- */
 public class Etagere {
-    private static int CountCode=1;
-    private int Code;
-    private String Domaine;
-    private int CapaciteMax;
-    ArrayList<Livre> Livres;
-    
-    Etagere(String domaineLivre, int capMax) {
-        Domaine = domaineLivre;
-        CapaciteMax = capMax;
-        Code = CountCode;
-        CountCode++;
+    private static int countCode = 1;
+    private int code;
+    private String domaine;
+    private int capaciteMax;
+    private ArrayList<Livre> livres;
+
+    public Etagere(String domaineLivre, int capMax) {
+        this.domaine = domaineLivre;
+        this.capaciteMax = capMax;
+        this.code = countCode++;
+        this.livres = new ArrayList<>();
     }
-    
-    Etagere(String domaineLivre, int capMax, int codeL){
-        Domaine = domaineLivre;
-        CapaciteMax = capMax;
-        // il est supossé que l'utilisateur ne rentre pas de code déjà existant
-        Code = codeL;
-        if(CountCode < codeL){
-            CountCode = codeL+1;
+
+    public Etagere(String domaineLivre, int capMax, int codeL) {
+        this.domaine = domaineLivre;
+        this.capaciteMax = capMax;
+        this.code = codeL;
+        this.livres = new ArrayList<>();
+        if (countCode < codeL) countCode = codeL + 1;
+    }
+
+    // Ajouter / Supprimer un livre
+    public void ajouterLivre(Livre livre) {
+        if (livres.size() < capaciteMax) {
+            livres.add(livre);
+        } else {
+            System.out.println("L'étagère est pleine !");
         }
     }
-    
-    public void ajouterLivre(Livre livre){
-        Livres.add(livre);
-    }
-    
+
     public void supprimerLivre(int code) {
-        boolean trouver = false;
-        int cpt = 0;
-        while(cpt < Livres.size() && !trouver){
-            if(Livres.get(cpt).getCode() == code){
-                Livres.remove(cpt);
-                trouver = true;
-            }
-            else {
-                cpt++;
+        boolean trouve = false;
+        for (Livre l : livres) {
+            if (l.getCode() == code) {
+                livres.remove(l);
+                trouve = true;
+                break;
             }
         }
-        if(!trouver){
-            System.out.println("Le livre avec le code d'identification "+code+" n'a pas était trouvé");
+        if (!trouve) {
+            System.out.println("Le livre avec le code " + code + " n’a pas été trouvé.");
         }
     }
-    
+
+
+    // Q5. chercherPetitsLivres
+    public void chercherPetitsLivres(int seuil) {
+        boolean trouve = false;
+        for (Livre l : livres) {
+            if (l.getNbPages() < seuil) {
+                System.out.println(l);
+                trouve = true;
+            }
+        }
+        if (!trouve) {
+            System.out.println("Aucun livre avec moins de " + seuil + " pages.");
+        }
+    }
+
+    // Q6. afficherNouveauxLivres
+    public void afficherNouveauxLivres(int anneeActuelle) {
+        boolean trouve = false;
+        for (Livre l : livres) {
+            if (l.getAnneeEdition() == anneeActuelle) {
+                System.out.println("Titre : " + l.getTitre() + " | Code : " + l.getCode());
+                trouve = true;
+            }
+        }
+        if (!trouve) {
+            System.out.println("Aucun livre édité en " + anneeActuelle + ".");
+        }
+    }
+
+    // Q7. changerNbPages
+    public boolean changerNbPages(int codeLivre, int nbPages) {
+        if (nbPages <= 0) return false;
+        for (Livre l : livres) {
+            if (l.getCode() == codeLivre) {
+                l.setNbPages(nbPages);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Q8. afficherLivres
+    public void afficherLivres() {
+        if (livres.isEmpty()) {
+            System.out.println("Aucun livre sur l'étagère.");
+            return;
+        }
+        for (Livre l : livres) {
+            System.out.println(l);
+        }
+    }
+
+    // Q9. trierParNbPages
+    public void trierParNbPages() {
+        livres.sort(Comparator.comparingInt(Livre::getNbPages));
+    }
+
+    // Q10. chercherTitresParAuteur
+    public String[] chercherTitresParAuteur(String auteur) {
+        ArrayList<String> titres = new ArrayList<>();
+        for (Livre l : livres) {
+            if (l.getAuteur().equalsIgnoreCase(auteur)) {
+                titres.add(l.getTitre());
+            }
+        }
+        if (titres.isEmpty()) {
+            System.out.println("Aucun livre trouvé pour l'auteur " + auteur + ".");
+        }
+        return titres.toArray(new String[0]);
+    }
+
+    // Méthodes utilitaires
     public int sommePages() {
         int somme = 0;
-        
-        for(Livre livre : Livres){
-            somme = somme+livre.getnbPage();
+        for (Livre l : livres) {
+            somme += l.getNbPages();
         }
-        
         return somme;
     }
-    
-    public String chercherLivreParTitre(String titre){
-        boolean trouver = false;
-        int cpt = 0;
-        String code = "Le livre recherché n'a pas était trouvé";
-        
-        while(cpt < Livres.size() && !trouver){
-            if(Livres.get(cpt).getTire() == titre){
-                trouver = true;
-                code = ""+Livres.get(cpt).getCode();
-            }
-            else{
-                cpt++;
+
+    public String chercherLivreParTitre(String titre) {
+        for (Livre l : livres) {
+            if (l.getTitre().equalsIgnoreCase(titre)) {
+                return String.valueOf(l.getCode());
             }
         }
-        return code;
+        return "Le livre recherché n’a pas été trouvé.";
     }
 }
--- TP2/src/tp2/Livre.java
+++ TP2/src/tp2/Livre.java
@@ -6,20 +6,70 @@ package tp2;
 
 /**
  *
- * @author jerem
+ * @author jerem & yoann
  */
-public class Livre {
 
-    
-    public int getnbPage(){
-        return 100;
+public class Livre {
+    private String titre;
+    private int code;
+    private String auteur;
+    private int nbPages;
+    private int anneeEdition;
+
+    public Livre(String titre, int code, String auteur, int nbPages, int anneeEdition) {
+        this.titre = titre;
+        this.code = code;
+        this.auteur = auteur;
+        this.nbPages = nbPages;
+        this.anneeEdition = anneeEdition;
     }
-    
-    public int getCode(){
-        return 2;
+
+    public String getTitre() {
+        return titre;
     }
-    
-    public String getTire(){
-        return "";
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public int getNbPages() {
+        return nbPages;
+    }
+
+    public int getAnneeEdition() {
+        return anneeEdition;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+
+    public void setNbPages(int nbPages) {
+        this.nbPages = nbPages;
+    }
+
+    public void setAnneeEdition(int anneeEdition) {
+        this.anneeEdition = anneeEdition;
+    }
+
+    @Override
+    public String toString() {
+        return "Livre [Titre = " + titre
+                + ", Code = " + code
+                + ", Auteur = " + auteur
+                + ", NbPages = " + nbPages
+                + ", Année = " + anneeEdition + "]";
     }
 }
