3fe59e367994e0c37ebadde9d68bc0b2b8926693
emarielouise972 <marielouiseesteban90@gmail.com>
2025-11-02 14:55:50 +0100

    partie 4

--- TP2/src/tp2/TP2.java
+++ TP2/src/tp2/TP2.java
@@ -8,13 +8,13 @@ package tp2;
  *
  * @author jerem
  */
+
 public class TP2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
